449a7b83b79ddb8b6c45f88df294a7301a60b513
Jabberghast <st189193@stud.uni-stuttgart.de>
2025-01-07 12:10:54 +0100

    finished task B5T1a

--- blatt05/aufgabe1.py
+++ blatt05/aufgabe1.py
@@ -21,8 +21,33 @@ def populateHashTable(n):
         for i in range(j, j * 6 + 1):
             probabilitynk(j, i)
 
-print(hashTable)
 populateHashTable(5)
-print(hashTable)
 
+distributionW_5 = []
+
+#add all values from hashTable with a key that starts with 5 to distributionW_5
+for key in hashTable:
+    if key.startswith("5"):
+        distributionW_5.append(hashTable[key])
+
+#plot the distribution as a bar chart
+#x-axis: shift everything by 5
+plt.bar(range(5, 5*6+1), distributionW_5)
+plt.title("Wahrscheinlichkeitsverteilung von W_5")
+plt.savefig("../pics/distributionW_5.png")
+plt.show()
+
+#plot the accumulated distribution as a line chart
+#extend the lines to the box of the plot
+
+accumulatedDistributionW_5 = [0]
+for i in range(1, len(distributionW_5)):
+    accumulatedDistributionW_5.append(accumulatedDistributionW_5[i-1] + distributionW_5[i])
+
+plt.step(range(5, 5*6 +1), accumulatedDistributionW_5, where='post', color="blue")
+plt.scatter(range(5, 5*6+1), accumulatedDistributionW_5)
+plt.xticks(range(5, 5*6 + 1))
+plt.title("Verteilungsfunktion von W_5")
+plt.savefig("../pics/accumulatedDistributionW_5.png")
+plt.show()
 
