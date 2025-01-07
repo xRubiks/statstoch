3bcac44755d3bf3f514eb4bba21fa52af973fad8
Jabberghast <st189193@stud.uni-stuttgart.de>
2025-01-07 14:44:51 +0100

    task B5T1 almost finished

--- blatt05/aufgabe1.py
+++ blatt05/aufgabe1.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 
-hashTable = {}
+#stores the probabilities of the points with n dice
+dice_points = {}
 
+#probability of 1 dice with k points
 for i in range(6):
-    hashTable["1" + str(i+1)] = 1/6
+    dice_points[(1, i + 1)] = 1 / 6
 
+#probability of n dice with k points
 def probabilitynk(n, k):
     result = 0
     for i in range(6):
-        if hashTable.get(str(n-1) + str(k-(i+1))):
-            result += hashTable.get(str(n-1) + str(k-(i+1)))
+        if dice_points.get((n - 1, k - (i + 1))):
+            result += dice_points.get((n - 1, k - (i + 1)))
         else:
             result += 0
-    hashTable[str(n) + str(k)] = (1/6) * result
+    dice_points[(n, k)] = (1 / 6) * result
 
+#populate the hashTable with all points with 1-n dice
 def populateHashTable(n):
     if n == 1:
         return
@@ -21,25 +25,23 @@ def populateHashTable(n):
         for i in range(j, j * 6 + 1):
             probabilitynk(j, i)
 
-populateHashTable(5)
+populateHashTable(40)
 
 distributionW_5 = []
 
 #add all values from hashTable with a key that starts with 5 to distributionW_5
-for key in hashTable:
-    if key.startswith("5"):
-        distributionW_5.append(hashTable[key])
+for key in dice_points:
+    if key[0] == 5:
+        distributionW_5.append(dice_points[key])
 
 #plot the distribution as a bar chart
 #x-axis: shift everything by 5
 plt.bar(range(5, 5*6+1), distributionW_5)
 plt.title("Wahrscheinlichkeitsverteilung von W_5")
 plt.savefig("../pics/distributionW_5.png")
-plt.show()
+plt.close()
 
 #plot the accumulated distribution as a line chart
-#extend the lines to the box of the plot
-
 accumulatedDistributionW_5 = [0]
 for i in range(1, len(distributionW_5)):
     accumulatedDistributionW_5.append(accumulatedDistributionW_5[i-1] + distributionW_5[i])
@@ -49,5 +51,25 @@ plt.scatter(range(5, 5*6+1), accumulatedDistributionW_5)
 plt.xticks(range(5, 5*6 + 1))
 plt.title("Verteilungsfunktion von W_5")
 plt.savefig("../pics/accumulatedDistributionW_5.png")
-plt.show()
+plt.close()
 
+k_max = []
+for i in range(2,41):
+    distribution = {}
+    accumulatedDistribution = {}
+    k_star = 0
+    for key in dice_points:
+        if key[0] == i:
+            distribution[key[1]] = dice_points[key]
+
+    accumulatedDistribution_pre = 0
+    for key in distribution:
+        accumulatedDistribution[key] = accumulatedDistribution_pre + distribution[key]
+        accumulatedDistribution_pre = accumulatedDistribution[key]
+    for key in accumulatedDistribution:
+        if accumulatedDistribution[key] <= 0.05:
+            k_star = key
+    k_max.append(k_star)
+
+for k_star in k_max:
+    print("k* für n = " + str(k_max.index(k_star) + 2) + ": " + str(k_star))
